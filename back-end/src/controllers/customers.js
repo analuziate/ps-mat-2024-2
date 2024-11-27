9923aed066078aba540ef1fcc1498998ea707724
Ana Luzia <analuzia.te@gmail.com>
2024-11-27 09:04:10 -0300

    "TRABALHO 2"

--- back-end/src/controllers/customers.js
+++ back-end/src/controllers/customers.js
@@ -1,54 +1,72 @@
-import { Prisma } from '@prisma/client'
 import prisma from '../database/client.js'
+import Customer from '../models/customer.js'
+import { ZodError } from 'zod'
 
-const controller = {} //Objeto vazio
+const controller = {}     // Objeto vazio
 
 controller.create = async function (req, res) {
   try {
+
+    // Chama a validação do Zod para o cliente
+    Customer.parse(req.body)
+
     await prisma.customer.create({ data: req.body })
 
-    //HTTP 201: Created
+    // HTTP 201: Created
     res.status(201).end()
   }
   catch (error) {
     console.error(error)
 
-    //HTTP 500: Internal server error
-    res.status(500).end()
+    // Se for erro de validação do Zod retorna
+    // HTTP 422: Unprocessable Entity
+    if (error instanceof ZodError) res.status(422).send(error.issues)
+
+    // HTTP 500: Internal Server Error
+    else res.status(500).end()
   }
 }
 
-controller.retriveAll = async function (req, res) {
+controller.retrieveAll = async function (req, res) {
   try {
     const result = await prisma.customer.findMany({
-      orderBy: [{ name: 'asc' }],
+      orderBy: [
+        { name: 'asc' }
+      ],
       include: {
         cars: req.query.include === 'cars'
       }
     })
-    res.send(result).end()
-  } catch (error) {
+
+    // HTTP 200: OK (implícito)
+    res.send(result)
+  }
+  catch (error) {
     console.error(error)
+
+    // HTTP 500: Internal Server Error
     res.status(500).end()
   }
 }
 
-controller.retriveOne = async function (req, res) {
+controller.retrieveOne = async function (req, res) {
   try {
     const result = await prisma.customer.findUnique({
       where: { id: Number(req.params.id) },
-      include: { cars: get.query.include === 'cars' }
+      include: {
+        cars: req.query.include === 'cars'
+      }
     })
 
-    //Encontrou -> retorna HTTP 200: OK (implícito)
-    if (result) res.send(result).end()
-    //Não Encontrou -> retorna HTTP 404: Not Found
+    // Encontrou ~> retorna HTTP 200: OK (implícito)
+    if (result) res.send(result)
+    // Não encontrou ~> retorna HTTP 404: Not Found
     else res.status(404).end()
   }
   catch (error) {
     console.error(error)
 
-    //HTTP 500: Internal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).end()
   }
 }
@@ -60,37 +78,38 @@ controller.update = async function (req, res) {
       data: req.body
     })
 
-    //Encontrou e atualizou -> HTTP 204: No Content
+    // Encontrou e atualizou ~> HTTP 204: No Content
     if (result) res.status(204).end()
-    //Não encontrou(e não atualizou) -> HTTP 404: Not Found
+    // Não encontrou (e não atualizou) ~> HTTP 404: Not Found
     else res.status(404).end()
   }
   catch (error) {
     console.error(error)
 
-    //HTTP 500: Internal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).end()
   }
 }
 
 controller.delete = async function (req, res) {
   try {
-    const result = await prisma.customer.delete({
+    await prisma.customer.delete({
       where: { id: Number(req.params.id) }
     })
 
-    //Encontrou e excluiu -> HTTP 204: No Content
+    // Encontrou e excluiu ~> HTTP 204: No Content
     res.status(204).end()
   }
   catch (error) {
     if (error?.code === 'P2025') {
-      //Não encontrou e não excluiu -> HTTP 404: Not Found
+      // Não encontrou e não excluiu ~> HTTP 404: Not Found
       res.status(404).end()
     }
     else {
+      // Outros tipos de erro
       console.error(error)
 
-      // HTTP 500 -> Internal Server Error
+      // HTTP 500: Internal Server Error
       res.status(500).end()
     }
   }
